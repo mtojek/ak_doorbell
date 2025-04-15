c070ac035b72c9e33816226c87539fa85a6575dc
Marcin Tojek <marcin@coder.com>
2025-04-15 20:23:20 +0200

    WIP

--- src/lv_port/display.cpp
+++ src/lv_port/display.cpp
@@ -3,6 +3,14 @@
 void lv_port_disp_init() {
   log::debug("lv_port_disp_init", "Initialize display.");
 
+  log::debug("lv_port_disp_init", "ak_tde_open()");
+  int r = ak_tde_open();
+  if (r != ERROR_TYPE_NO_ERROR) {
+    log::error("lv_port_disp_init", "ak_tde_open failed with code: " + r);
+    return;
+  }
+
+  log::debug("lv_port_disp_init", "fbdev_init()");
   fbdev_init();
 
   lv_disp_buf_init(&disp_buf, buf1, nullptr, DISP_BUF_SIZE);
--- src/lv_port/display.hpp
+++ src/lv_port/display.hpp
@@ -6,6 +6,12 @@
 
 #include "lv_drivers/display/fbdev.h"
 
+extern "C" {
+#include "anyka/ak_common.h"
+#include "anyka/ak_common_graphics.h"
+#include "anyka/ak_tde.h"
+}
+
 #define DISP_BUF_SIZE (LV_HOR_RES_MAX * 4 * LV_VER_RES_MAX)
 static lv_disp_buf_t disp_buf;
 static lv_color_t buf1[DISP_BUF_SIZE];
